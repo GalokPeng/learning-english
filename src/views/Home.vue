36e64503dfce612363cf3d7d4a13448462c87076
galokpeng <galokpeng@gmail.com>
2023-10-20 20:55:29 +0800

    增添描述信息

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
     <van-search placeholder="请输入搜索关键词"></van-search>
-    等待开发<br>
-    翻译功能<br>
-    开发初衷：背单词，不想看那么多广告又下载APP <br>
+    开发初衷：背单词，不想看那么多广告又下载APP <br/>
+    等待开发：<br/>
+    1、翻译功能<br/>
+    2、单词记忆方法<br/>
+    3、个人中心<br/>
+    4、...... <br/>
+    实现功能：<br/>
+    1、每日记词：随机20个单词
+    2、各类别考试单词表（待完善）
+
+
   </div>
 </template>
 <script setup lang="ts">
